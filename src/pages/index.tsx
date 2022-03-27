1e2a4afbcc3a7e3e0ddab2194126cb4945898da6
Yan Kairalla <yanmkairalla@hotmail.com>
2022-03-26 21:42:28 -0300

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <button className='button is-primary'>
+        Tesntando
+      </button>
+    </div>
+  )
+}
+
+export default Home
